56de0ff1247abb40703f9aef42894958951f962c
Alexey <qwe810@yandex.ru>
2024-04-01 18:18:18 +0700

    Commit 2

--- template/main.cpp
+++ template/main.cpp
@@ -1,52 +1,17 @@
 #include<iostream>
 #include<vector>
 #include<Windows.h>
-#include<string>
-
-//template<class T>
-//class simple_array
-//{
-//public:
-//    simple_array(int elements_count) : elements(new T[elements_count]), num(elements_count) {}
-//
-//    T getElement(int inIndex) const
-//    {
-//        return elements[inIndex];
-//    }
-//
-//    void setElement(int inIndex, T inValue)
-//    {
-//        elements[inIndex] = inValue;
-//    }
-//
-//    ~simple_array()
-//    {
-//        delete[] elements;
-//    }
-//
-//private:
-//    T* elements = nullptr;
-//    int num = 0;
-//};
-
-//void gotoxy(int x, int y)
-//{
-//    COORD p = { x, y };
-//    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), p);
-//}
 
 
+ template <typename T>
+ T print(T a)
+{
+    std::cout << "[IN]:   " << a << std::endl;
+    std::cout << "[OUT]:  " << a * a << std::endl;
+    return 0;
+}
 
-
-//template <typename T>
-//T print(T a)
-//{
-//    std::cout << "[IN]:   " << a << std::endl;
-//    std::cout << "[OUT]:  " << a * a << std::endl;
-//    return 0;
-//}
-
-//template<>
+template<>
 void const print(std::vector<int> const a)
 {
     std::cout << "[IN]:   ";
@@ -72,27 +37,6 @@ int main() {
     std::vector<int> myVector = { -1, 4, 8 };
 
     print(myVector);
-
-
-
-
-
-
-
-
-
-
-    // simple_array<char> a(4);
-
-    /* gotoxy(19, 11);
-     std::cout << "Проверка";
-     Sleep(2000);*/
-     //int age;
-     //double weight;
-     //std::cout << "Введите возраст и вес: ";
-     //std::cin >> age >> weight;
-     //std::cout << "Возраст: " << age << "\t Вес: " << weight << std::endl;
-
 
 
 
